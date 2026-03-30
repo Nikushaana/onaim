620dd9c6fca120a7bfd690a7a19e463688e849e6
Nikushaana <nikanashvili@gmail.com>
2026-03-30 19:19:13 +0400

    fix: added vercel domain for cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://onaim-one.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
